06e77ef3c6a1436e2cabb8e20b772078774ebe15
StormHooper <116904616+StormHooper@users.noreply.github.com>
2023-11-02 15:56:24 -0700

    Updated some stuff
    More closely follows instructions and corrected out-of-bound (map) bugs.

---
+++ Lab 10 - Dungeons and Monsters/Lab 10 - Dungeons and Monsters/hero.py
@@ -0,0 +1,43 @@
+from random import randint
+
+from entity import Entity
+from map import Map
+
+
+class Hero(Entity):
+  def __init__(self, name):
+    super().__init__(name, 25)
+    self._loc = [0, 0]
+
+  @property
+  def loc(self):
+    return self._loc
+
+  def attack(self, entity):
+    damage = randint(2, 5)
+    entity.take_damage(damage)
+    return f"{self.name} attacks a {entity.name} for {damage} damage."
+
+  def go_north(self):
+    if self.loc[0] > 0:
+      self.loc[0] -= 1
+      return Map()[self.loc[0]][self.loc[1]]
+    return "o"
+
+  def go_south(self):
+    if self.loc[0] < len(Map()) - 1:
+      self.loc[0] += 1
+      return Map()[self.loc[0]][self.loc[1]]
+    return "o"
+
+  def go_east(self):
+    if self.loc[1] < len(Map()[self.loc[1]]) - 1:
+      self.loc[1] += 1
+      return Map()[self.loc[0]][self.loc[1]]
+    return "o"
+  
+  def go_west(self):
+    if self.loc[1] > 0:
+      self.loc[1] -= 1
+      return Map()[self.loc[0]][self.loc[1]]
+    return "o"
